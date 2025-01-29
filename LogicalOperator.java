640e60d05dba717fb1c28bb1abe1499cc20cc5dd
Abhishek Chavhan <abhishekchavhan61047@gmail.com>
2025-01-29 16:49:10 +0530

    logical operator day-03
    Pratice program's Logical operators

---
+++ LogicalOperator.java
@@ -0,0 +1,23 @@
+public class Main {
+	public static void main(String[] args) {
+//				int a = 3, b = 7, c = 9;
+//		if ((a < b && b < c) || (a > c)) {
+//			System.out.println("Condition True");
+//		} else {
+//			System.out.println("Condition False");
+//		}
+
+//int totalBalance = 1000, withDraw = 500;
+//		if (withDraw <=  totalBalance && withDraw > 0) {
+//				System.out.println(" Transaction successful");
+//			} else {
+//				System.out.println(" Transaction Unsuccessful");
+//			}
+
+// find max num using ternary operator
+//	int a = 10, b = 20;
+//	int max = (a > b) ? a : b;
+//	System.out.println("Max: " + max);
+	
+	}
+}
