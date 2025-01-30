7bd2ac830762085557f8a45804e52f46c4b99d62
Abhishek Chavhan <abhishekchavhan61047@gmail.com>
2025-01-30 17:12:07 +0530

    Add files via upload
    Toggle button logic

---
+++ ToggleBtn.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		boolean isOn = false;
+
+		while (true) {
+			System.out.println(" Enter a number 0 = exit or 1 = toggle btn : ");
+			int num = sc.nextInt();
+
+			if (num == 0) {
+				System.out.println(" exiting...");
+				break;
+			} else if (num == 1) {
+				isOn = !isOn;
+				System.out.println(isOn ? "ON " : "OFF");
+
+			} else {
+				System.out.println("Enter a number 1 = exit or 1= on ");
+
+			}
+			sc.close();
+		}
+		
+	}
+}
