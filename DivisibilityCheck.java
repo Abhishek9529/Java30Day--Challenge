37fee1e60a4558b93052856cf65bcdecd96b2f37
Abhishek9529 <abhishekchavhan61047@gmail.com>
2025-01-31 23:45:04 +0530

    Add New File

---
+++ DivisibilityCheck.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class DivisibilityCheck {
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+
+// System.out.println("Enter any Number : ");
+// int num = sc.nextInt();
+// boolean isDivisible = (num % 5 == 0 || num % 6 == 0) && !(num % 5 == 0 && num
+// % 6 == 0);
+// System.out.println(isDivisible);
+
+
+// 2. check Valid Tringle
+// int s1 = 3, s2 = 4, s3 = 5;
+// boolean isValid = (s1 + s2 > s3) && (s2 + s3 > s1) && (s1 + s3 > s2);
+// System.out.println(isValid ? "is Valid" : "is Not Valid") ;
+
+
+//3.  check input number is 2 digit
+// int num = 40;
+// boolean isTwoDigit = (num >= 10) && (num <= 99);
+// System.out.println(isTwoDigit ? " is Two Digit " : " is not two digit");
+sc.close();
+}
+}
