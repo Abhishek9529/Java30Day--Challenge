4d42103f411aed88ce0bf09b102d315fa47373f5
Abhishek9529 <abhishekchavhan61047@gmail.com>
2025-02-01 18:00:39 +0530

    update code

--- ComparisonOperator.java
+++ ComparisonOperator.java
@@ -1,18 +1,28 @@
 
-// 1. Speed Limit Check
 import java.util.Scanner;
 
 public class ComparisonOperator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Car Speed : ");
-        int speed = sc.nextInt();
 
-        // Using Ternary Operator 
-        System.out.println(speed < 60 ? "Safe" : (speed <= 80 ? "Caution" : "Over Speed"));
+        // 1. Speed Limit Check
+        // System.out.println("Enter Car Speed : ");
+        // int speed = sc.nextInt();
+        // Using Ternary Operator
+        // System.out.println(speed < 60 ? "Safe" : (speed <= 80 ? "Caution" : "Over
+        // Speed"));
 
-        // Using boolean data types 
-        // boolean isSafe = (speed < 60), isCaution = (speed >= 60 && speed <= 80), isOverSpeed = (speed > 80);
-        // System.out.println((isSafe ? "Safe" : (isCaution ? "Caution" : (isOverSpeed ? "OverSpeed" : "high"))));
+        // Using boolean data types
+        // boolean isSafe = (speed < 60), isCaution = (speed >= 60 && speed <= 80),
+        // isOverSpeed = (speed > 80);
+        // System.out.println((isSafe ? "Safe" : (isCaution ? "Caution" : (isOverSpeed ?
+        // "OverSpeed" : "high"))));
+
+        // 2. Age Comparison for Competition
+        // int age1 = 28, age2 = 45, age3 = 50;
+        // check if age1 is youngest or not
+        // System.out.println(age1 < age2 && age1 < age3 ? "Youngest" : "Not the youngest");
+
     }
+
 }
