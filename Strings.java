cbbe1b31ea4f5060e79186c82c045bc607734dc1
Abhishek9529 <abhishekchavhan61047@gmail.com>
2025-02-03 20:49:38 +0530

    Add new File

---
+++ Strings.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class Strings {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    // Strings are used for storing text.
+    // A String variable contains a collection of characters surrounded by double quotes:
+    System.out.println("Enter Your Name : ");
+    String name = sc.nextLine();
+    // String name = "abhi";
+    System.out.print("Hello " + name);
+    }
+}
