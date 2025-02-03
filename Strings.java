4a0ec9157e062655d391e9cee153edb7e20ac7d3
Abhishek9529 <abhishekchavhan61047@gmail.com>
2025-02-03 21:02:36 +0530

    update code

--- Strings.java
+++ Strings.java
@@ -6,16 +6,21 @@ public class Strings {
     Scanner sc = new Scanner(System.in);
     // Strings are used for storing text.
     // A String variable contains a collection of characters surrounded by double quotes:
-    System.out.println("Enter Your Name : ");
-    String name = sc.nextLine();
+    // System.out.println("Enter Your Name : ");
+    // String name = sc.nextLine();
     // String name = "abhi";
-    System.out.print("Hello " + name);
+    // System.out.print("Hello " + name);
 
     // Length()
-    System.out.println("Length : " + name.length());
+    // System.out.println("Length : " + name.length());
 
     // There are many string methods available, for example toUpperCase() and toLowerCase():
-    System.out.println("in Uppercase : "+name.toUpperCase());
-    System.out.println("in Lowercase : "+name.toLowerCase());
+    // System.out.println("in Uppercase : "+name.toUpperCase());
+    // System.out.println("in Lowercase : "+name.toLowerCase());
+    
+
+    // The indexOf() method returns the index (the position) of the first occurrence of a specified text in a string (including whitespace):
+    // String txt = "java is a very easy language";
+    // System.out.println(txt.indexOf("easy"));
     }
 }
