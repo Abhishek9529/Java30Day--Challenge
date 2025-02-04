adcb2c89d98a9a7de85116a7bf4b240f9f9d50d7
Abhishek Chavhan <abhishekchavhan61047@gmail.com>
2025-02-04 17:36:49 +0530

    Add files via upload

---
+++ Functions.java
@@ -0,0 +1,13 @@
+// function in java
+public class Main {
+	public static void main(String[] args) {
+		String firstName = "jon";
+		String secondName = " doe";
+		String fullName = concatinate(firstName, secondName);
+System.out.println(fullName);
+		}
+	public static String concatinate(String  str1,String str2) {
+		//System.out.println(str1 + str2);
+		return str1 + str2;
+	}
+}
