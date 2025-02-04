68c7d21f5da3089d5ab4adaebea25d3502ed359b
Abhishek Chavhan <abhishekchavhan61047@gmail.com>
2025-02-04 18:22:17 +0530

    Update Functions.java

--- Functions.java
+++ Functions.java
@@ -2,16 +2,25 @@
 public class Main {
 	public static void main(String[] args) {
 
-                int num1 = 10, square = 0;
+                float fNum1 = 12.0f, fNum2 = 8.0f, total;
+		total = sum(fNum1,  fNum2);
+		System.out.println(total);
+
+		int num1 = 10, square = 0;
 		square = squareOfNum(num1);
 		System.out.println(square);
+
 
 		String firstName = "jon", secondName = " doe", fullName = "";
 		fullName = concatinate(firstName, secondName);
 		System.out.println(fullName);
 	}
 
-        // this function return square of number
+// this function return sum of numbers
+	public static float sum(float fNum1, float fNum2) {
+		return fNum1 + fNum2;
+	}
+// this function return square of number
 	public static int squareOfNum(int num) {
 		return num * num ;
 	}
