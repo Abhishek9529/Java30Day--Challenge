63581ea4cbd5fc703557510199f3bb28d105c6f2
Abhishek Chavhan <abhishekchavhan61047@gmail.com>
2025-02-04 17:48:08 +0530

    Update Functions.java

--- Functions.java
+++ Functions.java
@@ -1,13 +1,24 @@
 // function in java
 public class Main {
 	public static void main(String[] args) {
-		String firstName = "jon";
-		String secondName = " doe";
-		String fullName = concatinate(firstName, secondName);
-System.out.println(fullName);
-		}
-	public static String concatinate(String  str1,String str2) {
+
+                int num1 = 10, square = 0;
+		square = squareOfNum(num1);
+		System.out.println(square);
+
+		String firstName = "jon", secondName = " doe", fullName = "";
+		fullName = concatinate(firstName, secondName);
+		System.out.println(fullName);
+	}
+
+        // this function return square of number
+	public static int squareOfNum(int num) {
+		return num * num ;
+	}
+
+	// this function return concate string
+	public static String concatinate(String  str1, String str2) {
 		//System.out.println(str1 + str2);
-		return str1 + str2;
+		return str1 + str2;	
 	}
 }
