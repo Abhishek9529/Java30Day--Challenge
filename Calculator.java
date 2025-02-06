69b9e7e515a17e185246c43e9f692e26b40c855b
Abhishek Chavhan <abhishekchavhan61047@gmail.com>
2025-02-06 22:53:50 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+	
+	static void calculate(int num1, int num2){
+	   System.out.println("Addition : " + (num1 + num2) ) ;
+	   System.out.println("Subtract : " + (num1 - num2) ) ;
+	   System.out.println("Multiply : " + (num1 * num2) ) ;
+	     System.out.println("Division : " + (num1 / num2) ) ;
+	}
+	
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	System.out.println("enter 1st number : ");
+	int num1 = sc.nextInt();	
+System.out.println("enter 2nd number : ");
+	int num2 = sc.nextInt();
+		calculate(num1, num2);
+	}
+
+}
