af64a1cc297a91839c17ee0df963ad11ba0c9185
Abhishek Chavhan <abhishekchavhan61047@gmail.com>
2025-02-18 13:24:35 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,15 +1,22 @@
 public class Fibonacci {
-	public static void  fabnacci(int num) {
-		int a = 0, b = 1;
-		System.out.print(0 + ", " + 1);
-		for (int i = 3; i <= num; i++) {
-			int fab = a + b;
-			a = b ;
-			b = fab ;
-			System.out.print(", " + (fab));
+	public static void  fibonacci(int num) {
+		if (num == 1) {
+			System.out.println(0);
+		}
+		if (num >= 2) {
+			int a = 0, b = 1;
+			System.out.print(0 + ", " + 1);
+			if (num > 2) {
+				for (int i = 2; i < num; i++) {
+					int fab = a + b;
+					a = b ;
+					b = fab ;
+					System.out.print(", " + (fab));
+				}
+			}
 		}
 	}
 	public static void main(String[] args) {
-		fabnacci(5);
+		fibonacci(10);
 	}
 }
