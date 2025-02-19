3e794c31f97ab3813e9205e20377e40e1806a4dd
Abhishek Chavhan <abhishekchavhan61047@gmail.com>
2025-02-19 20:50:48 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,22 +1,25 @@
 public class Fibonacci {
 	public static void  fibonacci(int num) {
+		if (num == 0) {
+			System.out.println("invalid number, number should be greater then 0");
+			return;
+		}
 		if (num == 1) {
 			System.out.println(0);
+			return;
 		}
-		if (num >= 2) {
-			int a = 0, b = 1;
-			System.out.print(0 + ", " + 1);
-			if (num > 2) {
-				for (int i = 2; i < num; i++) {
-					int fab = a + b;
-					a = b ;
-					b = fab ;
-					System.out.print(", " + (fab));
-				}
-			}
+		StringBuilder sb = new StringBuilder();
+		int a = 0, b = 1;
+    sb.append(a).append(",").append(b);
+		for (int i = 2; i < num; i++) {
+			int fab = a + b;
+			a = b ;
+			b = fab ;
+			sb.append(",").append(fab);
 		}
+		System.out.print(sb.toString());
 	}
 	public static void main(String[] args) {
-		fibonacci(8);
+		fibonacci(2);
 	}
 }
