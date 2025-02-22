06b96e74a580de6f5858214904e362c7b48580b1
Abhishek Chavhan <abhishekchavhan61047@gmail.com>
2025-02-22 21:27:26 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+public class Array {
+	public static void main(String[] args) {
+
+		//Arrays are used to store multiple values in a single variable, instead of declaring separate variables for each value.
+		int[] arr = {10,20,30,40,50};
+		System.out.println(Arrays.toString(arr));
+	}
+}
