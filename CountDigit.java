f685bddd1711369c4e353d51c9c6ce454a9671ea
Abhishek9529 <abhishekchavhan61047@gmail.com>
2025-02-25 21:54:41 +0530

    Update Code

--- CountDigit.java
+++ CountDigit.java
@@ -1,18 +1,34 @@
 public class CountDigit {
-	// this program find the power of the given a on digit of the given num 
-	public static void main(String[] args) {
-		int num = 1234;
-		int a = 3;
+	public static int countNumber(int num){
 		int count = 0;
-		while(num >0){
-		num = num / 10;
-		count ++;
+		while (num > 0) {
+			num = num / 10;
+			count ++;
 		}
-		int cube = 1;
-		System.out.println(count);
-		for (int i =1; i <= count; i++){		    
-		    cube *= a;	    
+		return count;
+	}
+
+	public static int armstrong(int num){
+		int count = countNumber(Math.abs(num));
+		int digit = 0, total = 0, temp = Math.abs(num);
+		while (temp > 0) {
+			int power = 1;
+			digit = temp % 10;
+			for (int i =1; i <= count; i++){
+			power *= digit;
+			}
+			total += power;
+			temp = temp / 10;
 		}
-		System.out.println(cube);
+		return total;
+	}
+	public static void main(String[] args) {
+		int num = -9474;
+		int result = armstrong(num);
+		if (result == Math.abs(num)) {
+			System.out.println(num + " is an  Armstrong Number,  total : " + result );
+		}else{
+			System.out.println(num + " is not an Armstrong Number,  total : " + result );
+		}
 	}
 }
