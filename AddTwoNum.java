c0f58cc8d3be8f34f0639d59dc8f02bf04b72d3f
Abhishek Chavhan <abhishekchavhan61047@gmail.com>
2025-09-30 07:04:57 +0530

    Add files via upload

---
+++ AddTwoNum.java
@@ -0,0 +1,11 @@
+//Use of obj to call function.
+public class Main {
+	void addTwoNum(int a, int b) {
+		//int a =10, b=20,c=a+b;
+		System.out.println(a + b);
+	}
+	public static void main(String[] args) {
+		Main mn = new Main();
+		mn.addTwoNum(100, 200);
+	}
+}
