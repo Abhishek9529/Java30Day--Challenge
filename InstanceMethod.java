9e4df7bb931827b1c740332aab08d512926a738d
Abhishek Chavhan <abhishekchavhan61047@gmail.com>
2025-10-05 16:24:30 +0530

    Add files via upload

---
+++ InstanceMethod.java
@@ -0,0 +1,10 @@
+public class Main {
+	static void greetFunc(){
+	    System.out.println("Hello guys");
+	    } 
+	public static void main(String[] args) {
+	    //System.out.println("Hello guys");
+//JavaClasses mn = new JavaClasses();
+		greetFunc();
+	}
+}
