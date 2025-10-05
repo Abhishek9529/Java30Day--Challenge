300ab8567b028eb013d474a5e2d7b28ae5cd30f6
Abhishek Chavhan <abhishekchavhan61047@gmail.com>
2025-10-05 16:36:24 +0530

    Add files via upload

---
+++ StaticMethod.java
@@ -0,0 +1,9 @@
+public class Main {
+	//Static methods in Java are used for operations that are associated with the class itself, rather than with any specific instance of the class.
+	static void addTwoNum(int x, int y) {
+	       System.out.println(x+y);
+	}
+	public static void main(String[] args) {
+      addTwoNum(5,10);
+	}
+}
